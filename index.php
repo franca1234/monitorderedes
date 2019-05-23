0277674721537d4aca99f8ce5ba82b546f9b6930
franca1234 <50890839+franca1234@users.noreply.github.com>
2019-05-23 19:30:16 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link rel="stylesheet" href="css/estilo.css">
+
+	<meta charset="UTF-8">
+	<title>Monitor de Redes </title>
+</head>
+<body>
+	<div class="container">
+	<style>
+		
+		
+		.botao{border:5px  yellow;}
+		.row{border:5px solid yellow;}
+		.input{border:4px solid green;}
+	</style>
+
+<h1>Monitor de Redes</h1>
+<form method="post" class="form-inline"></form>
+<div class="row">
+
+<div class="col">
+<input type="text" placeholder="Digite seu nome aqui!" name="nomecomputador" class="form-control">
+</div>
+<div class="col">
+<input type="text" name="ip" placeholder="Digite seu ip" class="form-control">
+</div>
+<input type="submit" class="btn btn-outline-secondary botao" value="salvar"/>
+<div class="col"></div>
+<div class="row"></div>
+
+</div>
+</form>
+</div>
+</body>
+</html>
