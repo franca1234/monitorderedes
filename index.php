e06870abe23847c1b939fca6fee20501960b328d
franca1234 <50890839+franca1234@users.noreply.github.com>
2019-06-05 19:26:28 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,38 +1,66 @@
-<!DOCTYPE html>
 <html>
 <head>
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-	<link rel="stylesheet" href="css/estilo.css">
-
-	<meta charset="UTF-8">
-	<title>Monitor de Redes </title>
+<meta charset="utf-8">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<script type="text/css"></script>
+<link rel="stylesheet" href="css/estilo.css">
+<!-- comentário -->
+<title>Monitor de Redes</title>
+<?php require_once	'action/db_connect.php'; ?>
 </head>
 <body>
-	<div class="container">
-	<style>
-		
-		
-		.botao{border:5px  yellow;}
-		.row{border:5px solid yellow;}
-		.input{border:4px solid green;}
-	</style>
-
+<div class="container">
 <h1>Monitor de Redes</h1>
-<form method="post" class="form-inline"></form>
+<form method="post" class="form-inline" action="action/insert.php">
 <div class="row">
 
 <div class="col">
-<input type="text" placeholder="Digite seu nome aqui!" name="nomecomputador" class="form-control">
+<input type="text" placeholder="Nome" name="nomecomputador" class="form-control">
 </div>
+
 <div class="col">
-<input type="text" name="ip" placeholder="Digite seu ip" class="form-control">
+<input type="text" placeholder="IP" name="ip" class="form-control">
 </div>
-<input type="submit" class="btn btn-outline-secondary botao" value="salvar"/>
-<div class="col"></div>
-<div class="row"></div>
+
+<div class="col">
+<input type="submit" class="btn btn-secondary" value="salvar" />
+</div>
 
 </div>
-</form>
+</form>,
+
+<?php
+//SQL para selecionar informações na base de dados 
+	$sql = "select * from computadores";
+	$result = $connect->query($sql);
+
+		while($dado = $result->fetch_assoc()) {
+			$nome = $dado['nome'];
+			$ip = $dado['ip'];
+			$id = $dado['id'];
+			echo $dado['nome'] . "-" . $dado['ip'] . "<br>
+			<a href='action/delete.php?id=".$id."'>Excluir</a>
+			"."<br>";
+		}
+
+?>
+
+<div class="row">
+<div class="col">
+<div class="bloco online">
+<b>192.168.0.1</b><br>
+ONLINE
 </div>
+<div class="bloco offline">
+<b>192.168.1.1</b><br>
+OFFLINE
+</div>
+</div>
+</div>
+
+
+
+</div>
+
 </body>
 </html>
